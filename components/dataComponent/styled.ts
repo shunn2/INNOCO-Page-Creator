1c10ae18e3a553fd8aa3290c0e7aafad0e143e0f
조성훈B <shoonchoi@smilegate.com>
2023-02-15 17:33:16 +0900

    💄 [SGSDEVCAMP-2284] 방명록 UI 개선

---
+++ components/dataComponent/styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const GuestContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 12px 0;
+`;
+
+export const WriterInfo = styled.div`
+  font-size: 12px;
+  margin: 6px 0;
+`;
+
+export const WriterName = styled.div`
+  font-weight: 600;
+`;
+
+export const WriteDate = styled.div`
+  color: #aaa;
+`;
